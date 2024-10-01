6dcd9ec09f039f3873b0a1152c66e006fbb36982
Emanuele Rinaldi <emanuele.rinaldi@databricks.com>
2024-10-01 10:11:25 -0400

    monai databricks client integration

---
+++ monailabel/datastore/databricks_client.py
@@ -0,0 +1,87 @@
+import requests
+import json
+from typing import Any, Dict, Iterator, List, Optional, Tuple, Union
+from io import BytesIO
+
+from pydicom import dcmread
+from pydicom.dataset import Dataset
+from dicomweb_client import DICOMwebClient
+
+from monailabel.interfaces.exception import MONAILabelError, MONAILabelException
+
+
+class DatabricksClient(DICOMwebClient):
+    def __init__(self, url:str, token:str, warehouse_id:str, table:str):
+        self.base_url = url + "/api/2.0"
+        self.warehouse_id = warehouse_id
+        self.http_path = "/sql/1.0/warehouses/" + warehouse_id
+        self.table = table
+        self.headers = {"Authorization": f"Bearer {token}"}
+
+    def search_for_series(self, search_filters):
+
+        filters_list = ['1=1']
+
+        if('Modality' in search_filters):
+            filters_list.append(f"meta:['00080060'].Value[0] = '{search_filters['Modality']}'")
+        
+        if('SeriesInstanceUID' in search_filters):
+            filters_list.append(f"meta:['0020000E'].Value[0] = '{search_filters['SeriesInstanceUID']}'")
+
+        filters_list = ' and '.join(filters_list)
+
+        data = {
+            "warehouse_id": f"{self.warehouse_id}",
+            "statement": f"""SELECT
+                meta:['0020000D'], meta:['0020000E']
+                FROM {self.table} where {filters_list}""",
+            "wait_timeout": "30s",
+            "on_wait_timeout": "CANCEL"
+        }
+
+        dataset = requests.post(self.base_url+"/sql/statements/", json=data, headers=self.headers).json()
+
+        if(dataset['status']['state'] == 'FAILED'):
+            raise MONAILabelException(dataset['status']['error']['error_code'],
+                                      dataset['status']['error']['message'])
+
+        to_return = []
+
+        if 'data_array' in dataset['result']:
+            for value in dataset['result']['data_array']:
+                obj = {
+                    '0020000D': json.loads(value[0]), #StudyInstanceUID
+                    '0020000E': json.loads(value[1])  #SeriesInstanceUID
+                }   
+                to_return.append(obj)
+
+        return to_return
+    
+    def retrieve_series(
+        self,
+        study_instance_uid: str,
+        series_instance_uid: str,
+        media_types: Optional[Tuple[Union[str, Tuple[str, str]], ...]] = None
+    ) -> List[Dataset]:
+        
+        data = {
+            "warehouse_id": f"{self.warehouse_id}",
+            "statement": f"""SELECT local_path FROM {self.table} where 
+            meta:['0020000E'].Value[0] = '{series_instance_uid}' and
+            meta:['0020000D'].Value[0] = '{study_instance_uid}'
+            """,
+            "wait_timeout": "30s",
+            "on_wait_timeout": "CANCEL"
+        }
+
+        dataset = requests.post(self.base_url+"/sql/statements/", json=data, headers=self.headers).json()
+        paths = [path[0] for path in dataset['result']['data_array']]
+
+        to_return = []
+
+        for path in paths:
+            content = requests.get(self.base_url+f"/fs/files{path}", headers=self.headers).content
+            ds = dcmread(BytesIO(content))
+            to_return.append(ds)
+        
+        return to_return
